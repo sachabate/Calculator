97815107cf62f03806d4ae59b4960cbb235af6d2
sachabate <abate@fieldsafesolutions.com>
2022-04-26 16:28:07 -0600

    feat: buttons print to calculator screen

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const decimalButton = document.getElementById('decimal');
 const equalsButton = document.getElementById('equals');
 
 let operator = null;
-let arg1 = 0;
-let arg2 = 0;
+let arg1 = '';
+let arg2 = '';
 
 expression.innerHTML = '';
-output.innerHTML = '';
+output.innerHTML = '0';
 
 equalsButton.addEventListener('click', evaluate);
 clearButton.addEventListener('click', clear);
@@ -22,27 +22,55 @@ allClearButton.addEventListener('click', allClear);
 decimalButton.addEventListener('click', addDecimal);
 
 operatorButtons.forEach((button) => {
-    button.addEventListener('click', () => setOperator(button.textContent))
+    button.addEventListener('click', () => setOperator(button.id, button.textContent))
 });
 
-
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => appendNumber(button.textContent))
+});
 
 function evaluate(operator, arg1, arg2) {
 
 }
 
 function clear() {
-    
+    output.textContent = '0';
 }
 
 function allClear() {
-    
+    operator = null;
+    expression.textContent = '';
+    output.textContent = '0';
+    arg1 = '';
+    arg2 = '';
 }
 
 function addDecimal() {
-    
+    if (output.textContent.includes('.')) {
+        return;
+    } else {
+        output.textContent += '.';
+    }
 }
 
-function setOperator() {
+function setOperator(operationText, newOperator) {
+    if (operator == null) {
+        operator = operationText;
+        expression.textContent += ' ' + output.textContent + ' ' + newOperator;
+        arg1 = output.textContent;
+        output.textContent = '0';
+    } else {
+        arg2 = output.textContent;
+        evaluate();
+    }
+}
 
+function appendNumber(number) {
+    if (output.textContent === '0') {
+        output.innerHTML = number;
+    } else if (output.textContent.length >= 13) {
+        return;
+    } else {
+        output.textContent += number;
+    }
 }
